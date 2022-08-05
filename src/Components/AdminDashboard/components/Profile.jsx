b34ff3eb2f4b72eee460c58eea9f1e85c735f34f
Showkiip <71689825+Showkiip@users.noreply.github.com>
2022-08-06 00:25:39 +0500

    Update Profile.jsx

--- src/Components/AdminDashboard/components/Profile.jsx
+++ src/Components/AdminDashboard/components/Profile.jsx
@@ -4,7 +4,7 @@ import image from "../../../assets/profile.jpeg";
 import { HiOutlineLocationMarker } from "react-icons/hi";
 import { useSelector } from "react-redux";
 import { imageURL } from "../../../config/baseURL";
-import { Button, Card, CardActions, CardContent, CardMedia, Modal, Typography } from "@mui/material";
+import { Button, Card, CardActions, CardContent, CardMedia, FormControl, InputLabel, Modal, Select, Stack, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import EditProfile from "./modal/EditProfile";
 
@@ -23,6 +23,10 @@ const style = {
 export default function Profile() {
 
 
+  const Input = styled('input')({
+    display: 'none',
+  })
+
 
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
@@ -36,58 +40,183 @@ export default function Profile() {
   return (
 
     <Card sx={{
-      maxWidth: 345,
+      width: '100%',
+      maxWidth: '80%',
       margin: 'auto',
-      marginTop: '10px',
-      marginBottom: '10px',
+      marginTop: '20px',
+      marginBottom: '20px',
+      padding: '20px',
+      borderRadius: '10px',
+      boxShadow: '0px 0px 2px rgba(0, 0, 0, 0.1)',
+      backgroundColor: '#fff',
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
       justifyContent: 'center',
-      textAlign: 'center',
-      borderRadius: '10px',
-      backgroundColor: '#fff',
-      padding: '10px',
-      border: '2px solid #000',
       position: 'relative',
       overflow: 'hidden',
-      cursor: 'pointer',
-      '&:hover': {
-        boxShadow: '10px 10px 10px #000',
-        border: '2px solid #000',
-        backgroundColor: '#fff',
-        cursor: 'pointer',
-      }
+      border: '1px solid #e0e0e0',
+
 
     }}>
-      <CardMedia
-        component="img"
-        height="300"
-        image={imageURL}
-        alt="green iguana"
-         />
-      
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {userProfile?.name}
-        </Typography>
-        <CardActions>
-          <Typography size="small">{userProfile?.email}</Typography>
-        </CardActions>
-        <CardActions>
-          <Typography size="small">{userProfile?.profile_url}</Typography>
-        </CardActions>
+      <CardContent  >
+        <Stack alignItems="center"
+          justifyContent="center" gap="30px" position="relative">
+          <Box
+            sx={{
+              color: "red",
+              position: 'absolute',
+              top: '-20px',
+              right: '0',
+              cursor: 'pointer',
+              fontWeight: 'bold',
+            }} >
+          </Box>
+          <Typography
+            variant="h4"
+            sx={{
+              fontWeight: 'bold',
+              color: '#ffc107',
+              textAlign: 'center',
+           
+            }}>
+
+            Edit Profile
+          </Typography>
+          <Box sx={{ minWidth: 300 }}>
+            <Box>
+              <img src={image} alt="image" style={{ width: "140px", height: "140px", borderRadius: "70px" }} />
+            </Box>
+            <Stack direction="row" alignItems="center" >
+              <label htmlFor="contained-button-file">
+                <Input
+                  accept="image/*"
+                  id="contained-button-file"
+                  type="file"
+
+                />
+                <Button variant="contained" component="span"
+                  color="primary"  >
+                  Upload Image
+                </Button>
+              </label>
+
+            </Stack>
+          </Box>
+          <Stack gap="30px" direction="row">
+            <Box sx={{ minWidth: 300 }}>
+              <TextField
+                label="name"
+                type="text"
+                placeholder='Enter Name'
+                focused
+
+              />
+            </Box>
+            <Box sx={{ minWidth: 300 }}>
+              <TextField
+                label="email "
+                type="email "
+                placeholder='Enter email '
+                focused
+
+              />
+            </Box>
+            <Box sx={{ minWidth: 300 }}>
+              <TextField
+                label="phone"
+                type="number"
+                placeholder='Enter phone'
+                focused
+
+              />
+            </Box>
+          </Stack>
+          <Stack gap="30px" direction="row">
+            <Box sx={{ minWidth: 300 }}>
+              <TextField
+                label="address"
+                type="textarea"
+                placeholder='Enter address Here'
+                focused
+
+              />
+            </Box>
+            <Box sx={{ minWidth: 300 }}>
+              <TextField
+                label="city"
+                type="text"
+                placeholder='Enter city'
+                focused
+
+              />
+            </Box>
+            <Box sx={{ minWidth: 300 }}>
+              <TextField
+                label="state"
+                type="text"
+                placeholder='Enter state'
+                focused
+
+              />
+            </Box>
+
+          </Stack>
+          <Stack gap="30px" direction="row">
+            <Box sx={{ minWidth: 300 }}>
+              <TextField
+                label="country"
+                type="textarea"
+                placeholder='Enter country Here'
+                focused
+
+              />
+            </Box>
+            <Box sx={{ minWidth: 300 }}>
+              <TextField
+                label="zip"
+                type="text"
+                placeholder='Enter zip'
+                focused
+
+              />
+            </Box>
+            <Box sx={{ minWidth: 300 }}>
+              <TextField
+                label="description"
+                type="text"
+                placeholder='Enter description'
+                focused
+
+              />
+            </Box>
+
+          </Stack>
+          <Stack gap="30px" direction="row">
+            <Box sx={{ minWidth: 300 }}>
+              <Button variant="contained" component="span"
+                color="primary"
+                // startIcon={<CloudUploadIcon />}
+                style={{
+                  backgroundColor: '#ffc107',
+                  borderRadius: '10px',
+                  color: '#fff',
+                  fontWeight: 'bold',
+                  padding: '10px',
+                  marginLeft: '10px',
+                  marginTop: '10px',
+                  marginBottom: '10px',
+                  border: '1px solid #ffc107',
+                  cursor: 'pointer',
+                }}
+              >
+                Edit Profile
+              </Button>
+            </Box>
+          </Stack>
+        </Stack>
       </CardContent>
-      <CardActions>
-
-        <Button size="small" onClick={() => { setOpen(true) }}>Learn More</Button>
-      </CardActions>
-
-      <EditProfile open={open} setOpen={setOpen} />
-
     </Card>
-
-
 
   );
 }
